657a47be68274223d5fdf4d6136c27fe265ab3bf
Jonathan Krone <jkrone@vt.edu>
2016-03-31 22:05:08 -0500

    (cleanup) Wahoo! Deleted an unused file!

--- client/components/BottomNavBar/BottomButton.js
+++ client/components/BottomNavBar/BottomButton.js
@@ -28,13 +28,13 @@ export function BottomButton({ isWaitingForMatch, isConfirmed, isMatched, isPick
             </div>
           : isMatched && !isPickedUp
             ? <Chat
-              user_id={user_id}
-              isDriver={isDriver}
-              match={match}
-              isPickedUp={isPickedUp}
-              friends={friends}
-              messages={messages}
-            />
+                user_id={user_id}
+                isDriver={isDriver}
+                match={match}
+                isPickedUp={isPickedUp}
+                friends={friends}
+                messages={messages}
+              />
             : isDriver
               ? <h4>Thanks for taking {friendMatch.name} home.</h4>
               : <h3>{friendMatch.name} is taking you home.</h3>

beac60022574a7981fbdc76248536deac72f2703
Grant Redfearn <grantredfearn@gmail.com>
2016-04-06 16:34:56 -0500

    test-get all rides rockin

--- test/server/test_models/rides_test.js
+++ test/server/test_models/rides_test.js
@@ -20,11 +20,30 @@ describe('Rides Models', function () {
       ride_rider: SeedObj.rider1.foreign_rider,
     };
 
+    var rides = yield Ride.createRide(attrs);
+    assert.typeOf(rides, 'object');
+    assert.typeOf(rides.ride_id, 'number');
+    assert.typeOf(rides.ride_driver, 'number');
+    assert.typeOf(rides.ride_rider, 'number');
+  });
+
+  it_('get all rides in ride table', function * () {
+    var attrs = {
+      ride_driver: SeedObj.driver1.foreign_driver,
+      ride_rider: SeedObj.rider1.foreign_rider,
+    };
+
     yield Ride.createRide(attrs);
     var rides = yield Ride.getRides();
-    console.log('~_~_~', rides);
+    var ride = rides[0];
     assert.typeOf(rides, 'array');
     assert.lengthOf(rides, 1);
-    assert.typeOf(rides[0], 'object');
+    assert.typeOf(ride, 'object');
+    assert.typeOf(ride.ride_id, 'number');
+    assert.typeOf(ride.ride_driver, 'number');
+    assert.typeOf(ride.completed, 'boolean');
+    assert.typeOf(ride.canceled, 'boolean');
   });
+
+
 });

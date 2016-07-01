2813524e4f169b9db669e3550160fee9cb953c57
Jonathan Krone <jkrone@vt.edu>
2016-07-01 17:50:45 -0500

    (fix) Fix login by extending the interval of an OAuth library error check. Errors have not been prevented, just made less likely.

--- client/containers/Login.js
+++ client/containers/Login.js
@@ -5,7 +5,6 @@ import { FacebookButton } from '../models/Facebook';
 export function LoginContainer() {
   return (
     <div className='auth'>
-      <FacebookButton />
       <GithubButton />
       <GoogleButton />
     </div>

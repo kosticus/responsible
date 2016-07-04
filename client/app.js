defb178257ed34f0f93cb2db8acbbd6288a63df6
Jonathan Krone <jkrone@vt.edu>
2016-07-04 16:49:02 -0500

    (feat) Learned that location data is not available through web workers (bummer), so I am instead breaking the location service into a configuration file for clarity.

--- client/app.js
+++ client/app.js
@@ -29,7 +29,9 @@ import { LoginContainer } from './containers/Login';
 
 import { socket } from './lib/socketConfig';
 import { configureListeners } from './lib/socketListeners';
+import geoWatch from './lib/locationWatcher';
 
+geoWatch();
 configureListeners(socket);
 
 const routes = <Route component={Root}>
@@ -51,72 +53,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('app')
 );
-
-/*
-    TODO: refactor this into a separate component or web worker
-*/
-
-import * as userActions from './actionCreators/user';
-import * as rideActions from './actionCreators/ride';
-
-const DirectionsService = new google.maps.DirectionsService();
-
-window.geoWatch = geoWatch;
-
-geoWatch();
-setInterval(geoWatch, 6000);
-function geoWatch() {
-  if (!navigator.geolocation) return;
-
-  navigator.geolocation.getCurrentPosition(function (data) {
-    let user = store.getState().get('user').toJS();
-    let ride = store.getState().get('ride').toJS();
-    let nextLocation = {
-      lat: data.coords.latitude,
-      lng: data.coords.longitude,
-    };
-
-    //!!! Removed for testing of socket new_location events!
-    if (false && user.location && !haveMoved(user.location, nextLocation, 2e-8)) {
-      // do nothing
-    } else {
-      if (!ride.match)
-        store.dispatch(userActions.setLocation(nextLocation));
-      else
-        store.dispatch(userActions.setLocation(nextLocation, ride.match));
-    }
-
-    if (!ride.match) return;
-    let destination = ride.match.location;
-    if (ride.isPickedUp) {
-      let friendRider = user.friends.find((friend) => friend.user_id === ride.match.user_id);
-
-      if (user.isRider) destination = user.profile.address;
-      else destination = friendRider.address;
-    }
-
-    // console.log('routing to:', destination);
-    DirectionsService.route({
-      origin: nextLocation,
-      destination: destination,
-      travelMode: google.maps.TravelMode.DRIVING,
-    }, function (result, status) {
-        store.dispatch(rideActions.setDirections(result));
-      }
-    );
-  });
-}
-
-function haveMoved(curLocation, nextLocation, tol) {
-  if (Math.abs(curLocation.lat - nextLocation.lat) > tol ||
-      Math.abs(curLocation.lng - nextLocation.lng) > tol)
-    return true;
-  else
-    return false;
-}
-
-/*
-  I (Jonathan) found this for running actions when the tab is closed.
-  We would also need to do this on refresh
-*/
-window.onbeforeunload = function() { /* do things here */ };
---
+++ client/lib/locationWatcher.js
@@ -0,0 +1,55 @@
+import { store } from './storeConfig';
+
+import * as userActions from '../actionCreators/user';
+import * as rideActions from '../actionCreators/ride';
+
+// index.html exposes google
+const DirectionsService = new google.maps.DirectionsService();
+
+export default navigator.geolocation.watchPosition.bind(null, updateLocation);
+navigator.geolocation.getCurrentPosition(updateLocation);
+
+function updateLocation({ coords }) {
+  if (!'geolocation' in navigator) {
+    console.error('Device location is not available on this browser');
+    return;
+  }
+  if (!coords) {
+    console.error('Device did not provide location information. Please report to team :~)');
+  }
+
+
+
+  const location = {
+    lat: coords.latitude,
+    lng: coords.longitude
+  };
+
+  const ride = store.getState().get('ride').toJS();
+  if (!ride.match) {
+    store.dispatch(userActions.setLocation(location));
+    return;
+  }
+
+  store.dispatch(userActions.setLocation(location, ride.match));
+  let destination = ride.match.location;
+
+  if (ride.isPickedUp) {
+    const user = store.getState().get('user').toJS();
+    const friendRider = user.friends.find((friend) => friend.user_id === ride.match.user_id);
+
+    if (user.isRider) destination = user.profile.address;
+    else destination = friendRider.address;
+  }
+
+
+  console.log('routing to:', destination);
+  DirectionsService.route({
+    origin: location,
+    destination: destination,
+    travelMode: google.maps.TravelMode.DRIVING,
+  }, function (result, status) {
+      store.dispatch(rideActions.setDirections(result));
+    }
+  );
+}
